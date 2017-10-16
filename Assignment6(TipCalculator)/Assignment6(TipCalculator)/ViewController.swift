279eb9958e7f289cea87469781708be7cbdd0254
kaori <kaori55194@gmail.com>
2017-10-15 18:45:55 -0700

    modify assignment6

--- Assignment6(TipCalculator)/Assignment6(TipCalculator)/ViewController.swift
+++ Assignment6(TipCalculator)/Assignment6(TipCalculator)/ViewController.swift
@@ -7,6 +7,7 @@ class ViewController: UIViewController {
     
     var tip = 0.0
     var sliderPersent = 0.0
+    var buttonTapped = false
     @IBOutlet weak var tipPercentageTextField: UITextField!
     @IBAction func calculateTip(_ sender: Any) {
         // input text varsion
@@ -17,12 +18,22 @@ class ViewController: UIViewController {
         print("calculateTip-tip\(tip)")
 //        tipAmountLabel.text = String(tip)
         tipAmountLabel.text = "calculated tip is \(String(tip))$"
+        buttonTapped = true
     }
     
     @IBOutlet weak var adjustTipPercentage: UISlider!
     @IBOutlet weak var sliderLabel: UILabel!
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         let sliderValue = Int(sender.value)
+        if (buttonTapped){
+            self.sliderPersent = Double(sliderValue)
+            print("changed silder value (if)")
+            let decimal = Double(sliderPersent) * 1/100
+            self.tip = Double(billAmountTextField.text!)! * decimal
+            print("calculateTip-tip\(tip)")
+            tipAmountLabel.text = "calculated tip is \(String(tip))$"
+        }
+        print("changed silder value ()")
         sliderLabel.text = "\(String(sliderValue)) %"
         self.sliderPersent = Double(sliderValue)
     }
@@ -30,25 +41,35 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         tipAmountLabel.text = "calculated tip is"
         
-        if adjustTipPercentage.isContinuous == true{
-            self.sliderPersent = UISlider.v
-        }
+        let currentValue = Int(adjustTipPercentage.value)
+        print("Slider changing to \(currentValue) ?")
         
-//        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
-//        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        self.hideKeyboardWhenTappedAround()
         
-        
-        func keyboardWillShow(notification: NSNotification) {
+        NotificationCenter.default.addObserver(self, selector:#selector(ViewController.keyboardWillShow(notification:)),name:NSNotification.Name.UIKeyboardWillShow ,object:nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+
+    // keyboard disable
+//    func textFieldShouldReturn(textField: UITextField) -> Bool {
+//        billAmountTextField.resignFirstResponder()
+//        return true
+//    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
             if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
-                print("HELLO")
+                
                 if self.view.frame.origin.y == 0{
                     self.view.frame.origin.y -= keyboardSize.height
+                    print("HELLO")
+                    print("keyboardSize\(keyboardSize.height)")
                 }
             }
         }
-        func keyboardWillHide(notification: NSNotification) {
+    @objc func keyboardWillHide(notification: NSNotification) {
             
             if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
                 if self.view.frame.origin.y != 0{
@@ -56,11 +77,21 @@ class ViewController: UIViewController {
                 }
             }
         }
-        
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+}
+// keyboard disable
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
 }
